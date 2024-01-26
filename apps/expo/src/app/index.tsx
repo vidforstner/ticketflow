51412e3a3a24cc958937cc34a457c4c086783922
Forsto2012 <forstner.vid@gmail.com>
2024-01-26 18:32:25 +0100

    Actually works

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -5,9 +5,10 @@ import { Stack } from "expo-router";
 export default function Index() {
   return (
     <SafeAreaView className="bg-[#1F104A]">
-      <Stack.Screen options={{ title: "Ticket Flow Scanner" }} />
+      <Stack.Screen options={{ title: "Ticket dd Flow Scanner" }} />
       <View className="h-full w-full p-4">
-        <Text>sdsdsd</Text>
+        <Text>sdsdsd arojdfdfdfack</Text>
+        <Text>sdsdsd arojdfdfdfack celo dela top</Text>
       </View>
     </SafeAreaView>
   );
