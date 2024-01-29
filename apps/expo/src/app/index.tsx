ce7db2424dbebf6b505454e31098d964a084290a
Forsto2012 <forstner.vid@gmail.com>
2024-01-29 21:01:20 +0100

    moved shit arround

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import { Button, Text, TouchableOpacity, View } from "react-native";
-import { Camera, CameraType } from "expo-camera/";
-import { Stack } from "expo-router";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Camera, CameraType, FlashMode } from "expo-camera";
+import { ArrowLeftRight, CheckCheck, Zap } from "lucide-react-native";
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
+  const [flash, setFlash] = useState(FlashMode.off);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
+    setScanned(true);
+    setData(data);
+  };
 
   if (!permission) return <View />;
 
@@ -25,19 +34,56 @@ export default function App() {
     );
   }
 
+  function toggleFlashMode() {
+    setFlash((current) =>
+      current === FlashMode.off ? FlashMode.on : FlashMode.off,
+    );
+  }
+
   return (
-    <View className="h-full justify-center">
-      <Stack.Screen options={{ title: "Ticket Flow Scanner" }} />
-      <Camera style={{ flex: 1 }} type={type}>
-        <View className="relative">
+    <View className="relative h-full justify-center">
+      <TouchableOpacity
+        className="absolute right-4 top-20 z-30 mx-auto"
+        onPress={toggleCameraType}
+      >
+        <ArrowLeftRight className="h-20 w-20 text-white" />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        className="absolute right-4 top-36 z-30 mx-auto"
+        onPress={toggleFlashMode}
+      >
+        <Zap className="h-20 w-20 text-white" />
+      </TouchableOpacity>
+      <BarCodeScanner
+        style={{ flex: 1 }}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        type={type}
+      />
+      {scanned && (
+        <View className="absolute bottom-0 w-full rounded-t-2xl border-t bg-black p-4 text-white">
+          <CheckCheck
+            className="mx-auto mb-8 mt-4 text-green-300"
+            height={90}
+            width={90}
+          />
+          <Text className="mx-auto text-3xl font-bold text-white">Valid</Text>
+          <Text className="mx-auto text-xl font-bold text-white">{data}</Text>
+
+          <Text className="mx-auto mt-4 text-white">
+            Purchased on: 20. 1. 2023
+          </Text>
+
           <TouchableOpacity
-            className="absolute right-4 top-20 mx-auto"
-            onPress={toggleCameraType}
+            className="mx-6 mb-12 mt-20 rounded bg-white"
+            onPress={() => setScanned(false)}
           >
-            <Text className="text-2xl font-bold text-white">Flip</Text>
+            <Text className="py-2 text-center text-2xl font-bold text-black">
+              Close
+            </Text>
           </TouchableOpacity>
         </View>
-      </Camera>
+      )}
     </View>
   );
 }
