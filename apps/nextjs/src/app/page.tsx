f539c77b513c9bb78c87d961b5cf495cdb83964b
Forsto2012 <forstner.vid@gmail.com>
2024-02-01 18:22:20 +0100

    kinda added shiut

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,10 +1,36 @@
-export default async function HomePage() {
+"use client";
+
+import Slider from "react-slick";
+
+export default function HomePage() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  };
   return (
-    <main className="container h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <div className="w-full max-w-2xl overflow-y-scroll"></div>
-        <div className="w-full max-w-2xl overflow-y-scroll"></div>
-      </div>
+    <main className="mt-16">
+      <section className="h-[1080px] overflow-x-hidden text-black">
+        <Slider {...settings}>
+          <div className="mx-2 h-[650px] bg-white">ssdsd </div>
+          <div className="mx-2 h-full bg-white">ssdsd </div>
+          <div className="mx-2 h-full bg-white">ssdsd </div>
+        </Slider>
+      </section>
     </main>
   );
 }
