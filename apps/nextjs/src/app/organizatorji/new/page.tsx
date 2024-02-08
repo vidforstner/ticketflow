bcbc7c46c65b4772765820c3cdc8f5e18a0e1326
Forsto2012 <forstner.vid@gmail.com>
2024-02-08 08:51:53 +0100

    kinda did something

--- apps/nextjs/src/app/organizatorji/new/page.tsx
+++ apps/nextjs/src/app/organizatorji/new/page.tsx
@@ -27,6 +27,16 @@ export default function Organizatorji() {
 
   const [location, setLocation] = useState("");
 
+  const [tickets, setTickets] = useState(
+    [] as {
+      name: string;
+      amount: number;
+      start: Date;
+      end: Date;
+      details: string;
+    }[],
+  );
+
   // Custom function to handle date or undefined for start date
   const handleSetStart = (date: Date | undefined) => {
     if (date) setStart(date);
@@ -54,6 +64,124 @@ export default function Organizatorji() {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
+            <div className="grid grid-cols-2 gap-x-4">
+              <Label>Začetek</Label>
+              <Label>Konec</Label>
+
+              <DateTimePicker date={start} setDate={handleSetStart} />
+
+              <DateTimePicker date={end} setDate={handleSetEnd} />
+            </div>
+            <div className="mt-4 grid grid-cols-2 gap-x-4">
+              <Label>Lokacija</Label>
+              <Label className="mb-1">Vrsta dogodka</Label>
+
+              <GooglePlacesAutocomplete
+                selectProps={{
+                  styles: {
+                    control: (provided, state) => ({
+                      ...provided,
+                      backgroundColor: "#171717", // Dark background for the input
+                      borderColor: state.isFocused ? "#262626" : "#262626", // Consistent border color
+                      color: "#FFFFFF", // Set text color to white
+                      boxShadow: state.isFocused ? "none" : provided.boxShadow, // Remove boxShadow on focus
+                      "&:hover": {
+                        borderColor: "#262626", // Maintain border color on hover
+                      },
+                      // Apply additional styles to ensure text color and remove outline
+                    }),
+                    input: (provided) => ({
+                      ...provided,
+                      color: "#FFFFFF", // This ensures the text inside the input is white
+                      "&:focus": {
+                        outline: "none", // Remove outline on focus
+                      },
+                    }),
+                    singleValue: (provided) => ({
+                      ...provided,
+                      color: "#FFFFFF", // This ensures the text inside the input is white
+                    }),
+                    menu: (provided) => ({
+                      ...provided,
+                      backgroundColor: "#171717", // Dark background for the dropdown
+                    }),
+                    option: (provided, state) => ({
+                      ...provided,
+                      backgroundColor: state.isSelected ? "#27272a" : "#171717", // Different background if option is selected
+                      color: "#FFFFFF", // Text color for options
+                      "&:hover": {
+                        backgroundColor: "#27272a", // Background color on hover
+                      },
+                    }),
+                  },
+                }}
+                apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+              />
+
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Izberi vrsto dogodka" />
+                </SelectTrigger>
+                <SelectContent className="border border-neutral-800">
+                  <SelectGroup>
+                    <SelectItem value="koncert">Koncert</SelectItem>
+                    <SelectItem value="festival">Festival</SelectItem>
+                    <SelectItem value="DJ">DJ Party</SelectItem>
+                    <SelectItem value="zabava">Zabava</SelectItem>
+                  </SelectGroup>
+                  <SelectSeparator />
+                  <SelectGroup>
+                    <SelectItem value="razstava">Razstava</SelectItem>
+                    <SelectItem value="film">Film</SelectItem>
+                    <SelectItem value="predstava">Predstava</SelectItem>
+                    <SelectItem value="stand-up">Stand-up komedija</SelectItem>
+                    <SelectItem value="kvizi">Kvizi</SelectItem>
+                  </SelectGroup>
+                  <SelectSeparator />
+                  <SelectGroup>
+                    <SelectItem value="konferenca">Konferenca</SelectItem>
+                    <SelectItem value="seminar">Seminar</SelectItem>
+                  </SelectGroup>
+                  <SelectSeparator />
+                  <SelectGroup>
+                    <SelectItem value="kolesarstvo">Kolesarstvo</SelectItem>
+                    <SelectItem value="kosarka">Košarka</SelectItem>
+                    <SelectItem value="paintball">Paintball</SelectItem>
+                    <SelectItem value="rafting">Rafting</SelectItem>
+                    <SelectItem value="tek-maraton">Tek / Maraton</SelectItem>
+                    <SelectItem value="turnirji">Turnirji</SelectItem>
+                    <SelectItem value="mma">MMA</SelectItem>
+                  </SelectGroup>
+                  <SelectSeparator />
+                  <SelectGroup>
+                    <SelectItem value="networking">Networking</SelectItem>
+                    <SelectItem value="delavnica">Delavnica</SelectItem>
+                    <SelectItem value="ostalo">Ostalo</SelectItem>
+                  </SelectGroup>
+                  <SelectSeparator />
+                  <SelectGroup>
+                    <SelectItem value="privatni-dogodek">
+                      Privatni dogodek
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </>
+        )}
+
+        {page === 1 && (
+          <>
+            <h1 className="mb-4 text-center text-2xl font-bold">Vstopnice</h1>
+
+            <Label htmlFor="ticketName">Ime vstopnice</Label>
+            <Input
+              id="ticketName"
+              className="mb-4"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
             <div className="grid grid-cols-2 gap-x-4">
               <Label>Začetek</Label>
               <Label>Konec</Label>
