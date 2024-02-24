4becdd6121801402cf4b1b9a95885f8e859d8437
Forsto2012 <forstner.vid@gmail.com>
2024-02-24 12:50:51 +0100

    basic data model

--- apps/nextjs/src/app/organizatorji/new/i/page.tsx
+++
@@ -1,9 +0,0 @@
-import EventForm from "../event-form";
-
-export default function Page() {
-  return (
-    <div className="container my-24 rounded-2xl bg-neutral-900 p-6 ">
-      <EventForm />
-    </div>
-  );
-}
