20f05331882cf6828a84a1e45361543a20f665ad
Forsto2012 <forstner.vid@gmail.com>
2024-02-26 00:03:59 +0100

    Made webhook route public

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -4,7 +4,13 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/prijava", "/registracija", "/api/uploadthing"],
+  publicRoutes: [
+    "/",
+    "/prijava",
+    "/registracija",
+    "/api/uploadthing",
+    "/api/webhook",
+  ],
 });
 
 export const config = {
