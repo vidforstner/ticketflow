ab9a1f85a011081386d4f1ddbbafe3d383f468c6
Forsto2012 <forstner.vid@gmail.com>
2024-02-28 22:01:17 +0100

    kinda works, but not really

--- apps/nextjs/src/app/organizatorji/new/event-action.ts
+++ apps/nextjs/src/app/organizatorji/new/event-action.ts
@@ -1,12 +1,21 @@
 "use server";
 
 import { action } from "@/lib/safe-action";
+import { auth } from "@clerk/nextjs";
 
 import { prisma } from "@acme/db";
 
-import { eventSchema } from "./event-form";
+import { eventSchema } from "./schema";
 
 export const createEvent = action(eventSchema, async (event) => {
+  const { userId } = auth();
+
+  const author = await prisma.user.findUnique({
+    where: {
+      clerkId: userId ?? "",
+    },
+  });
+
   //Create a Clerk organization
 
   await prisma.event.create({
@@ -26,6 +35,23 @@ export const createEvent = action(eventSchema, async (event) => {
       instagram: event.instagram,
       youtube: event.youtube,
       description: event.description,
+      ticketTypes: {
+        create: event.tickets.map((ticket) => ({
+          name: ticket.name,
+          amount: ticket.amount,
+          price: ticket.price,
+          startDate: ticket.start,
+          endDate: ticket.end,
+          details: ticket.details,
+        })),
+      },
+      eventMemberships: {
+        create: {
+          role: "ADMIN",
+          userId: author?.id ?? "",
+          clerkId: userId ?? "",
+        },
+      },
     },
   });
 
--- apps/nextjs/src/app/organizatorji/new/event-form.tsx
+++ apps/nextjs/src/app/organizatorji/new/event-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { z } from "zod";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { DateTimePicker } from "@/components/ui/date-time-picker";
@@ -36,89 +37,14 @@ import { XIcon } from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { useFieldArray, useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { EventType } from "@acme/db";
 
 import { Button } from "~/components/ui/button";
 import { Textarea } from "~/components/ui/textarea";
 import { UploadButton } from "~/utils/uploadthing";
 import { createEvent } from "./event-action";
+import { eventSchema } from "./schema";
 
 type EventFormValues = z.infer<typeof eventSchema>;
-
-const eventSchema = z.object({
-  name: z
-    .string()
-    .min(2, {
-      message: "Ime dogodka mora biti dolgo vsaj 2 znaka",
-    })
-    .max(50, {
-      message: "Ime dogodka ne sme biti daljše od 50 znakov",
-    }),
-  slug: z
-    .string()
-    .min(2)
-    .max(50)
-    .regex(
-      /^[a-zA-Z0-9-]*$/,
-      "URL dogodka lahko vsebuje le črke, številke in -",
-    ),
-  start: z.date(),
-  end: z.date(),
-  location: z.object({
-    label: z.string(),
-    value: z.object({
-      description: z.string(),
-      place_id: z.string(),
-      reference: z.string(),
-      structured_formatting: z.object({
-        main_text: z.string(),
-        secondary_text: z.string(),
-      }),
-      terms: z.array(
-        z.object({
-          offset: z.number(),
-          value: z.string(),
-        }),
-      ),
-      types: z.array(z.string()),
-    }),
-  }),
-  type: z.nativeEnum(EventType),
-  tickets: z.array(
-    z.object({
-      name: z
-        .string()
-        .min(2, {
-          message: "Ime vstopnice mora biti dolgo vsaj 2 znaka",
-        })
-        .max(50),
-      amount: z
-        .number()
-        .int({
-          message: "Količina mora biti celo število",
-        })
-        .positive({
-          message: "Količina mora biti pozitivno število",
-        }),
-      price: z.number().nonnegative(),
-      start: z.date(),
-      end: z.date(),
-      details: z.string().optional(),
-    }),
-  ),
-  imageUrl: z.string().url({
-    message: "Vnesite veljavno sliko",
-  }),
-  phone: z.string().optional(),
-  email: z.string().optional(),
-  website: z.string().url().optional(),
-  facebook: z.string().url().optional(),
-  instagram: z.string().url().optional(),
-  youtube: z.string().url().optional(),
-  description: z.string().optional(),
-});
 
 const defaultTicketValues: Partial<EventFormValues["tickets"][0]> = {
   name: "",
@@ -727,5 +653,3 @@ export default function EventForm() {
     </Form>
   );
 }
-
-export { eventSchema };
