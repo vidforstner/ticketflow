ab8de737138b272ca63d01c78c8f512665731332
Forsto2012 <forstner.vid@gmail.com>
2024-02-28 09:21:53 +0100

    Fixed errors

--- apps/nextjs/src/app/organizatorji/new/event-form.tsx
+++ apps/nextjs/src/app/organizatorji/new/event-form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { z } from "zod";
 import { useState } from "react";
 import Image from "next/image";
 import { DateTimePicker } from "@/components/ui/date-time-picker";
@@ -36,13 +35,85 @@ import { Editor } from "@tinymce/tinymce-react";
 import { XIcon } from "lucide-react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { EventType } from "@acme/db";
 
 import { Button } from "~/components/ui/button";
 import { Textarea } from "~/components/ui/textarea";
 import { UploadButton } from "~/utils/uploadthing";
-import { eventSchema } from "./event-action";
 
 type EventFormValues = z.infer<typeof eventSchema>;
+
+const eventSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Ime dogodka mora biti dolgo vsaj 2 znaka",
+    })
+    .max(50, {
+      message: "Ime dogodka ne sme biti daljše od 50 znakov",
+    }),
+  slug: z
+    .string()
+    .min(2)
+    .max(50)
+    .regex(/^[^\s]*$/, "No spaces are allowed"),
+  start: z.date(),
+  end: z.date(),
+  location: z.object({
+    label: z.string(),
+    value: z.object({
+      description: z.string(),
+      place_id: z.string(),
+      reference: z.string(),
+      structured_formatting: z.object({
+        main_text: z.string(),
+        secondary_text: z.string(),
+      }),
+      terms: z.array(
+        z.object({
+          offset: z.number(),
+          value: z.string(),
+        }),
+      ),
+      types: z.array(z.string()),
+    }),
+  }),
+  type: z.nativeEnum(EventType),
+  tickets: z.array(
+    z.object({
+      name: z
+        .string()
+        .min(2, {
+          message: "Ime vstopnice mora biti dolgo vsaj 2 znaka",
+        })
+        .max(50),
+      amount: z
+        .number()
+        .int({
+          message: "Količina mora biti celo število",
+        })
+        .positive({
+          message: "Količina mora biti pozitivno število",
+        }),
+      price: z.number().nonnegative(),
+      start: z.date(),
+      end: z.date(),
+      details: z.string().optional(),
+    }),
+  ),
+  imageUrl: z.string().url({
+    message: "Vnesite veljavno sliko",
+  }),
+  phone: z.string().optional(),
+  email: z.string().optional(),
+  website: z.string().url().optional(),
+  facebook: z.string().url().optional(),
+  instagram: z.string().url().optional(),
+  youtube: z.string().url().optional(),
+  description: z.string().optional(),
+});
 
 const defaultTicketValues: Partial<EventFormValues["tickets"][0]> = {
   name: "",
@@ -609,3 +680,5 @@ export default function EventForm() {
     </Form>
   );
 }
+
+export { eventSchema };
