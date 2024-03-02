6a4b2379817d4487c19a902037ad8c56a9927f45
Forsto2012 <forstner.vid@gmail.com>
2024-03-02 11:49:53 +0100

    create event

--- apps/nextjs/src/app/organizatorji/new/event-action.ts
+++ apps/nextjs/src/app/organizatorji/new/event-action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { action } from "@/lib/safe-action";
-import { auth } from "@clerk/nextjs";
+import { auth, clerkClient } from "@clerk/nextjs";
 
 import { prisma } from "@acme/db";
 
@@ -16,9 +16,14 @@ export const createEvent = action(eventSchema, async (event) => {
     },
   });
 
+  const organization = await clerkClient.organizations.createOrganization({
+    name: event.name,
+    createdBy: userId ?? "",
+  });
+
   await prisma.event.create({
     data: {
-      organisationId: event.slug,
+      organisationId: organization.id,
       name: event.name,
       slug: event.slug,
       startDate: event.start,
@@ -53,60 +58,7 @@ export const createEvent = action(eventSchema, async (event) => {
     },
   });
 
-  /*
-   ticketTypes: {
-        create: event.tickets.map((ticket) => ({
-          name: ticket.name,
-          amount: ticket.amount,
-          price: ticket.price,
-          startDate: ticket.start,
-          endDate: ticket.end,
-          details: ticket.details,
-        })),
-      },
-  
-  */
-
   //Create a Clerk organization
-  /*
-  await prisma.event.create({
-    data: {
-      organisationId: event.slug,
-      name: event.name,
-      slug: event.slug,
-      startDate: event.start,
-      endDate: event.end,
-      placeId: event.location.value.place_id,
-      type: event.type,
-      imageUrl: event.imageUrl,
-      phone: event.phone,
-      email: event.email,
-      website: event.website,
-      facebook: event.facebook,
-      instagram: event.instagram,
-      youtube: event.youtube,
-      description: event.description,
-      ticketTypes: {
-        create: event.tickets.map((ticket) => ({
-          name: ticket.name,
-          amount: ticket.amount,
-          price: ticket.price,
-          startDate: ticket.start,
-          endDate: ticket.end,
-          details: ticket.details,
-        })),
-      },
-      eventMemberships: {
-        create: [
-          {
-            role: "ADMIN",
-            userId: author?.id ?? "",
-            clerkId: userId ?? "",
-          },
-        ],
-      },
-    },
-  }); */
 
   //Sends Email
 });
